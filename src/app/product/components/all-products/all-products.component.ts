2e38ea3c031dc2b171d707725ddf0a3bea6d35f9
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-01-23 21:39:33 +0200

    get all products from the api

--- src/app/product/components/all-products/all-products.component.ts
+++ src/app/product/components/all-products/all-products.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { productService } from '../../product.service';
+interface product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
 @Component({
   selector: 'app-all-products',
   templateUrl: './all-products.component.html',
-  styleUrl: './all-products.component.scss'
+  styleUrl: './all-products.component.scss',
 })
-export class AllProductsComponent {
-
+export class AllProductsComponent implements OnInit {
+  constructor(private productServ: productService) {}
+  products: product[] = [];
+  ngOnInit(): void {
+    this.productServ.getAllProducts().subscribe((res) => {
+      this.products = res;
+    });
+  }
 }
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AllProductsComponent } from './components/all-products/all-products.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { HttpClientModule } from '@angular/common/http';
+import { productService } from './product.service';
 
 @NgModule({
   declarations: [AllProductsComponent, ProductDetailComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
+  providers: [productService],
 })
 export class ProductModule {}
